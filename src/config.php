5899cfea874cb31453115c39f05248da3c5d51f3
WolHenn <wol@streetart-museum.de>
2026-04-20 17:28:19 +0200

    Zweiter Cleanup: node_modules und Bilder aus Repository hoffentlich entfernt

--- src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'WEBSITE_APP_KEY' => 'Mein_super_geheimer_Website_Key_2026',
-    'DB_PASSWORD' => 'w0l@MySQL_Docker',
-    'DB_HOST' => 'streetart_mysql-container'
-];
