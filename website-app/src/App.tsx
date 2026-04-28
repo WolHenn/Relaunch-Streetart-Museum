26843a9b9d07dec281acca000144e86d32df6084
WolHenn <wol@streetart-museum.de>
2026-04-28 16:59:26 +0200

    Live-Website Grundgerüst hinzugefügt.

---
+++ website-app/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+export default function App() {
+    return (
+        <div className="p-8">
+            <h1 className="text-3xl font-bold">Hallo Welt – Streetart Museum Relaunch</h1>
+        </div>
+    );
+}
